92ca8335e77c98f800eaccb3907928a0968d6af0
bondanjs <bondanjodys@gmail.com>
2024-11-24 15:16:23 +0700

    Menambahkan middleware untuk mengautorisasi jika halaman tertentu hanya bisa diakses oleh user dengan role tertentu. Mendaftarkan middleware. Menggunakan middleware di route.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\UserRoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,11 +10,11 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'user.role:CANDIDATE'])->name('dashboard');
 
 Route::get('company/dashboard', function () {
     return view('frontend/company-dashboard.dashboard');
-})->middleware(['auth', 'verified'])->name('company/dashboard');
+})->middleware(['auth', 'verified', 'user.role:COMPANY'])->name('company/dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

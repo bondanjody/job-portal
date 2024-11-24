bd17513ff8e858db67a32c27524c5091145f373a
bondanjs <bondanjodys@gmail.com>
2024-11-24 13:16:53 +0700

    Menambahkan route untuk menangani redirect ke halaman dashboard company. Menambahkan percabangan pada saat autentikasi untuk mengenali role kemudian diarahkan ke halaman dashboard sesuai role. Menambahkan view dashboard untuk company.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('company/dashboard', function () {
+    return view('frontend/company-dashboard.dashboard');
+})->middleware(['auth', 'verified'])->name('company/dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
